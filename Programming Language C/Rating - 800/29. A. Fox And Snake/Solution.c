b06d9d35bb3745e59207f21ff4cf2e2f98cea3fa
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2024-08-20 11:58:22 +0600

    Add files via upload

---
+++ Programming Language C/Rating - 800/29. A. Fox And Snake/Solution.c
@@ -0,0 +1,19 @@
+//C Solution:
+#include <stdio.h>
+
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    for ( int i = 1; i <= n; i++ ){
+        for ( int j = 1; j <= m; j++ ){
+            if ( i % 2 != 0 || i % 2 == 0 && i % 4 != 0 && j == m || i % 4 == 0 && j == 1 ){
+                printf("#");
+            }
+            else printf(".");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
