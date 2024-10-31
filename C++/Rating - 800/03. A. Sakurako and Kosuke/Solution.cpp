41a5388a7086d497a531c95789477b5b96756212
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2024-10-31 20:52:04 +0600

    Create Solution.cpp

---
+++ C++/Rating - 800/03. A. Sakurako and Kosuke/Solution.cpp
@@ -0,0 +1,35 @@
+//C++ Solution:
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef double dou;
+
+#define endl "\n"
+#define PI (2.0 * acos(0.0))
+
+inline int ceil(int a, int b){
+return (a + b - 1) / b;
+}
+inline int abs (int a, int b){
+int d1 = a - b;
+if (d1 < 0) d1 *= -1;
+return d1;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while( t-- ){
+        int n;
+        cin >> n;
+
+        if( n % 2 == 0 ) cout << "Sakurako" << endl;
+        else cout << "Kosuke" << endl;
+    }
+    return 0;
+}
