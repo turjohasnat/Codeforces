dbe9fb0c58aa8befbb03e5bec2c537b4f9f7676e
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2024-11-11 09:13:45 +0600

    Create Solution.c

---
+++ C++/Rating - 900/01. B. Everyone Loves Tres/Solution.c
@@ -0,0 +1,48 @@
+//C++ Solution:
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef double dou;
+
+#define endl "\n"
+#define PI (2.0 * acos(0.0))
+
+inline int ceil(int a, int b){
+return (a + b - 1) / b;
+}
+inline int abs (int a, int b){
+int d1 = a - b;
+if (d1 < 0) d1 *= -1;
+return d1;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while( t-- ){
+        int n;
+        cin >>n;
+
+        if( n == 1 || n == 3 ){
+            cout << "-1" << endl;
+        }
+        else if( n % 2 == 0 ){
+            for( int i = 0; i < n-2; i++ ){
+                cout << "3";
+            }
+            cout << "66" << endl;
+        }
+        else{
+            for( int i = 0; i < n-5; i++ ){
+                cout << "3";
+            }
+            cout << "36366" << endl;
+        }
+    }
+    return 0;
+}
