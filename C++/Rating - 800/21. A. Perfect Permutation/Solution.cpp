baf3e1da55847118a20f0dd9fb1c393d53773270
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2024-12-06 10:07:43 +0600

    Create Solution.cpp

---
+++ C++/Rating - 800/21. A. Perfect Permutation/Solution.cpp
@@ -0,0 +1,37 @@
+//C++ Solution:
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef double dou;
+
+#define endl "\n"
+#define PI (2.0 * acos(0.0))
+
+inline int ceil(int a, int b){
+return (a + b - 1) / b;
+}
+inline int abs (int a, int b){
+int d1 = a - b;
+if (d1 < 0) d1 *= -1;
+return d1;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    if( n % 2 == 1 ) cout << "-1" << endl;
+    else{
+        cout << "2 1";
+        for( int i = 3; i < n; i += 2 ){
+            cout << " " << i + 1 << " " << i;
+        }
+    }
+    cout << endl;
+
+    return 0;
+}
