3d64fbfb8865b75de43f4d97e041176f340f210b
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2024-12-09 23:17:25 +0600

    Create Solution.cpp

---
+++ C++/Rating - 900/05. A. Even Odds/Solution.cpp
@@ -0,0 +1,33 @@
+//C++ Solution:
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef double dou;
+
+#define endl "\n"
+#define PI (2.0 * acos(0.0))
+
+inline int ceil(int a, int b){
+return (a + b - 1) / b;
+}
+inline int abs (int a, int b){
+int d1 = a - b;
+if (d1 < 0) d1 *= -1;
+return d1;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll n, k;
+    cin >> n >> k;
+
+    ll a;
+    if(k <= (n + 1) / 2) a = 2 * k - 1;
+    else a = 2 * (k - (n + 1) / 2);
+
+    cout << a << endl;
+    return 0;
+}
