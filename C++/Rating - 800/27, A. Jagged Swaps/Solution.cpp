ea416e6ac84e221b07983fdcb113556e8f29731f
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2024-12-12 17:34:00 +0600

    Create Solution.cpp

---
+++ C++/Rating - 800/27, A. Jagged Swaps/Solution.cpp
@@ -0,0 +1,41 @@
+//C++ Solution:
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef double dou;
+
+#define endl "\n"
+#define PI (2.0 * acos(0.0))
+
+inline int ceil(int a, int b){
+return (a + b - 1) / b;
+}
+inline int abs (int a, int b){
+int d1 = a - b;
+if (d1 < 0) d1 *= -1;
+return d1;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while( t-- ){
+        int n;
+        cin >> n;
+
+        vector<int> a(n);
+        for( int i = 0; i < n; i++ ){
+            cin >> a[i];
+        }
+
+        if( a[0] == 1 ) cout << "YES" << endl;
+        else cout << "NO" << endl;
+    }
+
+    return 0;
+}
