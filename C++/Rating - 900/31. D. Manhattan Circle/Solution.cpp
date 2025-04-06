a089e57092f28e8ee68303f39edf17f22eade104
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2025-04-06 23:17:12 +0600

    Create Solution.cpp

---
+++ C++/Rating - 900/31. D. Manhattan Circle/Solution.cpp
@@ -0,0 +1,85 @@
+//C++ Solution:
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int,int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef double dl;
+
+#define PB push_back
+#define F first
+#define S second
+#define MP make_pair
+#define all(a) (a).begin(),(a).end()
+#define sz(x) (int)x.size()
+#define mid(l,r) ((r+l)/2)
+#define left(node) (node*2)
+#define right(node) (node*2+1)
+#define mx_int_prime 999999937
+#define endl "\n"
+#define PI (2.0 * acos(0.0))
+#define MOD 1000000007
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define sqr(a) ((a) * (a))
+
+const double eps = 1e-9;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
+
+inline int ceil(int a, int b){
+return (a + b - 1) / b;
+}
+inline int abs (int a, int b){
+int d1 = a - b;
+if (d1 < 0) d1 *= -1;
+return d1;
+}
+
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    ll t;
+    cin >> t;
+
+    while(t--){
+        ll n, m;
+        cin >> n >> m;
+
+        ll row = 0, column = 0;
+        ll cnt = 0, previous = 0;
+        bool first = false;
+
+        for(ll i = 1; i <= n; i++){
+            for(ll j = 1; j <= m; j++){
+                char c;
+                cin >> c;
+
+                if(c == '#' && first == false){
+                    row = j;
+                    first = true;
+                }
+
+                if(c == '#')
+                    cnt++;
+            }
+
+            if(cnt >= previous){
+                column = i;
+                previous = cnt;
+            }
+            cnt = 0;
+        }
+        cout << column << " " << row << endl;
+    }
+    return 0;
+}
