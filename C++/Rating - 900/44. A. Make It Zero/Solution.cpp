936e3335e55d1383af5cb8247455ea5baa4bcdb3
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2025-04-30 21:01:09 +0600

    Create Solution.cpp

---
+++ C++/Rating - 900/44. A. Make It Zero/Solution.cpp
@@ -0,0 +1,62 @@
+//C++ Solution:
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef double dl;
+
+#define all(a) (a).begin(),(a).end()
+#define sz(x) (int)x.size()
+#define mid(l,r) ((r+l)/2)
+#define left(node) (node*2)
+#define right(node) (node*2+1)
+#define mx_int_prime 999999937
+#define endl "\n"
+#define PI (2.0 * acos(0.0))
+#define MOD 1000000007
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define sqr(a) ((a) * (a))
+
+const double eps = 1e-9;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
+
+inline int ceil(int a, int b){
+return (a + b - 1) / b;
+}
+inline int abs (int a, int b){
+int d1 = a - b;
+if (d1 < 0) d1 *= -1;
+return d1;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    ll t;
+    cin >> t;
+
+    while(t--){
+        ll n;
+        cin >> n;
+
+        ll a[n];
+        for(ll i = 0; i < n; i++)
+            cin >> a[i];
+        
+        if(n % 2 == 0){
+            cout << 2 << endl;
+            cout << 1 << " " << n << endl;
+            cout << 1 << " " << n << endl;
+        }
+        else{
+            cout << 4 << endl;
+            cout << 1 << " " << n-1 << endl;
+            cout << 1 << " " << n-1 << endl;
+            cout << n-1 << " " << n << endl;
+            cout << n-1 << " " << n << endl;
+        }
+    }
+    return 0;
+}
