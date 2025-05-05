315c10ddd4ac4527894a51aa369b06b975718354
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2025-05-05 12:01:16 +0600

    Create Solution.cpp

---
+++ C++/Rating - 900/54. A. AvtoBus/Solution.cpp
@@ -0,0 +1,53 @@
+//C++ Solution:
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef double dl;
+
+#define all(a) (a).begin(),(a).end()
+#define sz(x) (int)x.size()
+#define mid(l,r) ((r+l)/2)
+#define left(node) (node*2)
+#define right(node) (node*2+1)
+#define mx_int_prime 999999937
+#define endl "\n"
+#define PI (2.0 * acos(0.0))
+#define MOD 1000000007
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define sqr(a) ((a) * (a))
+
+const double eps = 1e-9;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
+
+inline int ceil(int a, int b){
+return (a + b - 1) / b;
+}
+inline int abs (int a, int b){
+int d1 = a - b;
+if (d1 < 0) d1 *= -1;
+return d1;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    ll t;
+    cin >> t;
+
+    while(t--){
+        ll n;
+        cin >> n;
+        
+        if(n < 4 || n % 2 == 1)
+            cout << -1 << endl;
+        else{
+            ll min_bus = (n + 5) / 6;
+            ll max_bus = n / 4;
+            cout << min_bus << " " << max_bus << endl;
+        }
+    }
+    return 0;
+}
