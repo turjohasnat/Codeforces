bb5c0daf833c65db35ed393d9e734f667b939e68
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2025-05-07 23:07:45 +0600

    Create Solution.cpp

---
+++ C++/Rating-1100/02. A. Parallelepiped/Solution.cpp
@@ -0,0 +1,47 @@
+//C++ Solution:
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef double dl;
+
+#define all(a) (a).begin(),(a).end()
+#define sz(x) (int)x.size()
+#define mid(l,r) ((r+l)/2)
+#define left(node) (node*2)
+#define right(node) (node*2+1)
+#define mx_int_prime 999999937
+#define endl "\n"
+#define PI (2.0 * acos(0.0))
+#define MOD 1000000007
+#define mem(a,b) memset(a, b, sizeof(a) )
+
+const double eps = 1e-9;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
+
+inline int ceil(int a, int b){
+return (a + b - 1) / b;
+}
+inline int abs (int a, int b){
+int d1 = a - b;
+if (d1 < 0) d1 *= -1;
+return d1;
+}
+
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll ab, bc, ca;
+    cin >> ab >> bc >> ca;
+
+    ll a = sqrt((ab * bc)/ca);
+    ll b = sqrt((bc * ca)/ab);
+    ll c = sqrt((ca * ab)/bc);
+
+    ll sum = 4 * (a + b + c);
+    cout << sum << endl;
+    return 0;
+}
