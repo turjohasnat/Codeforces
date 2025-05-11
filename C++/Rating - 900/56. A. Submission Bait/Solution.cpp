103d868e884fa1a7a0d0d072cd7bc925367cc1bd
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2025-05-11 10:22:41 +0600

    Update Solution.cpp

--- C++/Rating - 900/56. A. Submission Bait/Solution.cpp
+++ C++/Rating - 900/56. A. Submission Bait/Solution.cpp
@@ -1,53 +1,61 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-#define m_p make_pair
-#define all(x) (x).begin(),(x).end()
-#define sz(x) ((int)(x).size())
-#define fi first
-#define se second
+
 typedef long long ll;
-mt19937 rnd(chrono::steady_clock::now().time_since_epoch().count());
-mt19937 rnf(2106);
-const int N = 55;
 
-int n;
-int q[N];
+#define all(a) (a).begin(),(a).end()
+#define sz(x) (int)x.size()
+#define mid(l,r) ((r+l)/2)
+#define left(node) (node*2)
+#define right(node) (node*2+1)
+#define mx_int_prime 999999937
+#define endl "\n"
+#define PI (2.0 * acos(0.0))
+#define MOD 1000000007
+#define mem(a,b) memset(a, b, sizeof(a) )
 
-void solv()
-{
-    cin >> n;
-    for (int i = 1; i <= n; ++i)
-        q[i] = 0;
-    for (int i = 1; i <= n; ++i)
-    {
-        int x;
-        cin >> x;
-        ++q[x];
-    }
+const double eps = 1e-9;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
 
-    for (int i = 1; i <= n; ++i)
-    {
-        if (q[i] % 2 == 1)
-        {
-            cout << "YES\n";
-            return;
-        }
-    }
-    cout << "NO\n";
+inline int ceil(int a, int b){
+    return (a + b - 1) / b;
+}
+inline int abs (int a, int b){
+    int d1 = a - b;
+    if (d1 < 0) d1 *= -1;
+    return d1;
 }
 
-int main()
-{
-    #ifdef SOMETHING
-    freopen("input.txt", "r", stdin);
-    #endif
-    ios_base::sync_with_stdio(false), cin.tie(0);
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-    int tt = 1;
-    cin >> tt;
-    while (tt--)
-    {
-        solv();
+    ll t;
+    cin >> t;
+
+    while(t--){
+        ll n;
+        cin >> n;
+
+        vector<ll> a(n, 0);
+        for(auto i = 0; i < n; i++)
+            cin >> a[i];
+
+        map<ll, ll> m;
+        for(auto u : a)
+            m[u]++;
+
+        bool isAlice = false;
+        for(auto u : m){
+            if(u.second % 2 != 0)
+                isAlice = true;
+        }
+
+        if(isAlice)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
     }
     return 0;
 }
