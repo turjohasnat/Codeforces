b0dd597fc74e82218155c44aecf12b581b011272
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2025-05-12 21:20:35 +0600

    Create Solution.cpp

---
+++ C++/Rating - 900/76. B. Following the String/Solution.cpp
@@ -0,0 +1,51 @@
+//C++ Solution:
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef double dou;
+
+#define endl "\n"
+#define PI (2.0 * acos(0.0))
+
+inline int ceil(int a, int b){
+return (a + b - 1) / b;
+}
+inline int abs (int a, int b){
+int d1 = a - b;
+if (d1 < 0) d1 *= -1;
+return d1;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t;
+    cin >> t;
+
+    while (t--){
+        ll n;
+        cin >> n;
+
+        string s = "abcdefghijklmnopqrstuvwxyz";
+
+        map<char, ll> m;
+        for(auto u : s)
+            m[u] = 0;
+
+        vector<ll> a(n);
+        for(auto i = 0; i < n; i++){
+            cin >> a[i];
+            for(auto u : m){
+                if(u.second == a[i]){
+                    cout << u.first;
+                    m[u.first]++;
+                    break;
+                }
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
