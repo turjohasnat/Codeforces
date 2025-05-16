8701a01e5c9276bd0c7066fd928243598902d462
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2025-05-16 22:02:24 +0600

    Create Solution.cpp

---
+++ C++/Rating - 900/85. B. Good Arrays/Solution.cpp
@@ -0,0 +1,50 @@
+//C++ Solution:
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef double dou;
+
+#define endl "\n"
+#define PI (2.0 * acos(0.0))
+
+inline int ceil(int a, int b){
+return (a + b - 1) / b;
+}
+inline int abs (int a, int b){
+int d1 = a - b;
+if (d1 < 0) d1 *= -1;
+return d1;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t;
+    cin >> t;
+
+    while (t--){
+        ll n;
+        cin >> n;
+
+        vector<ll> a(n);
+        for(auto &u : a)
+            cin >> u;
+
+        ll sum = 0, cnt_1 = 0;
+        for(auto u : a){
+            if(u == 1)
+                cnt_1++;
+            
+            sum += u;
+        }
+
+        if(sum >= n + cnt_1 && n > 1)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+
+    }
+    return 0;
+}
