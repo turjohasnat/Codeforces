2b8112a797a4a58df65d740615888b206af2f54b
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2025-05-17 23:53:06 +0600

    Create Solution.cpp

---
+++ C++/Rating - 900/86. A. Magic Numbers/Solution.cpp
@@ -0,0 +1,43 @@
+//C++ Solution:
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef double dou;
+
+#define endl "\n"
+#define PI (2.0 * acos(0.0))
+
+inline int ceil(int a, int b){
+return (a + b - 1) / b;
+}
+inline int abs (int a, int b){
+int d1 = a - b;
+if (d1 < 0) d1 *= -1;
+return d1;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    string n;
+    cin >> n;
+    
+    ll magic = 0;
+    for(auto i = 0; i < n.size(); i++){
+        if(n[i] == '1' && n[i+1] == '4' && n[i+2] == '4')
+            magic += 3;
+        else if(n[i] == '1' && n[i+1] == '4')
+            magic += 2;
+        else if(n[i] == '1')
+            magic += 1;
+    }
+    if(magic == n.size()){
+        cout << "YES" << endl;
+    }
+    else 
+        cout << "NO" << endl;
+
+    return 0;
+}
