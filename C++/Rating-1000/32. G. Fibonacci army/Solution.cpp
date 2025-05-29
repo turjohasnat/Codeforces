441fbc481888bdc84985e0b4378b0289fafa3092
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2025-05-29 14:34:23 +0600

    Create Solution.cpp

---
+++ C++/Rating-1000/32. G. Fibonacci army/Solution.cpp
@@ -0,0 +1,41 @@
+x := File standardInput readLine asNumber
+a:=1
+if (x==1, a println)
+b:=2
+if (x==2, b println)
+c:=a+b
+if (x==3, c println)
+a:=b+c
+if (x==4, a println)
+b:=c+a
+if (x==5, b println)
+c:=a+b
+if (x==6, c println)
+a:=b+c
+if (x==7, a println)
+b:=c+a
+if (x==8, b println)
+c:=a+b
+if (x==9, c println)
+a:=b+c
+if (x==10, a println)
+b:=c+a
+if (x==11, b println)
+c:=a+b
+if (x==12, c println)
+a:=b+c
+if (x==13, a println)
+b:=c+a
+if (x==14, b println)
+c:=a+b
+if (x==15, c println)
+a:=b+c
+if (x==16, a println)
+b:=c+a
+if (x==17, b println)
+c:=a+b
+if (x==18, c println)
+a:=b+c
+if (x==19, a println)
+b:=c+a
+if (x==20, b println)
