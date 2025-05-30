5a5bbf4407b8ccaaf783a1409b1f58651039f6e7
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2025-05-30 13:39:18 +0600

    Create Solution.cpp

---
+++ C++/Rating-1000/39. B. Pair of Toys/Solution.cpp
@@ -0,0 +1,38 @@
+//C++ Solution:
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef double dou;
+
+#define endl "\n"
+#define PI (2.0 * acos(0.0))
+
+const double eps = 1e-9;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
+
+inline int ceil(int a, int b){
+return (a + b - 1) / b;
+}
+inline int abs (int a, int b){
+int d1 = a - b;
+if (d1 < 0) d1 *= -1;
+return d1;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll n, k;
+    cin >> n >> k;
+
+    ll low = max(1LL, k - n);
+    ll high = (k - 1) / 2;
+    ll ways = max(0LL, high - low + 1);
+
+    cout << ways << endl;
+    
+    return 0;
+}
