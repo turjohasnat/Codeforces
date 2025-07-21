d66e7f450ab5f43d709c70c6593108c384b29e73
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2025-07-21 19:28:15 +0600

    Create A._Twice.cpp

---
+++ Contest Problems Solutions/Codeforces Round 988 (Div. 3)/A._Twice.cpp
@@ -0,0 +1,57 @@
+//C++ Solution:
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef double dou;
+
+#define endl "\n"
+#define PI (2.0 * acos(0.0))
+
+inline int ceil(int a, int b){
+return (a + b - 1) / b;
+}
+inline int abs (int a, int b){
+int d1 = a - b;
+if (d1 < 0) d1 *= -1;
+return d1;
+}
+
+void solve();
+bool isVowel( char c );
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    solve();
+
+    return 0;
+}
+
+void solve(){
+    int t;
+    cin >> t;
+
+    while( t-- ){
+        int n;
+        cin >> n;
+
+        vector<int> v(n);
+        for( int i = 0; i < n; i++ ) cin >> v[i];
+
+        sort( v.begin(), v.end() );
+
+        int c = 0;
+        for( int i = 0; i < n; i++ ){
+            if(v[i] == v[i+1]){
+                c++;
+                i++;
+            }
+        }
+        cout << c << endl;
+    }
+}
+
+
+ 
